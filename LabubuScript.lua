37a032eb80d688bfba6b8a50a60e4d50e00526b7
xxxgamerxxxez-cmd <xxxgamerxxxez@gmail.com>
2025-08-14 16:23:05 +0300

    sda
    asd

---
+++ LabubuScript.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxgamerxxxez-cmd/Loadstring-script-chec2/refs/heads/main/Myscript.lua"))()
